db6adbf62bed5410e055df215e58ba46b06d48d0
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2023-06-08 18:40:25 +0200

    Add timeout to iostream

--- app/src/test/kotlin/kttp/net/SimpleTestServer.kt
+++ app/src/test/kotlin/kttp/net/SimpleTestServer.kt
@@ -1,35 +1,34 @@
 package kttp.net
 
 import java.net.ServerSocket
+import java.time.Duration
 
-class SimpleTestServer(port: Int) {
+class SimpleTestServer(port: Int, private val timeout: Int) {
 
     private val serverSocket = ServerSocket(port)
-    private lateinit var  ioStream: IOStream
+    private lateinit var ioStream: IOStream
 
 
-    fun start(){
+    fun start() {
         val socket = serverSocket.accept()
-
-        ioStream = IOStream(socket)
+        ioStream = IOStream(socket, Duration.ofSeconds(timeout.toLong()))
 
 
     }
 
-    fun stop(){
+    fun stop() {
         serverSocket.close()
-        if(::ioStream.isInitialized)
+        if (::ioStream.isInitialized)
             ioStream.close()
     }
 
-    fun readLine(): String{
+    fun readLine(): String {
         return ioStream.readLine()
     }
 
-    fun write(msg: String){
-        ioStream.writeln(msg)
+    fun write(msg: String) {
+        ioStream.println(msg)
     }
-
 
 
 }
