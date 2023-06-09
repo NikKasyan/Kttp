5819966bb7874baa96913d1619fc755fe21f0f54
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2023-06-09 13:25:30 +0200

    Add test for HttpRequestHandler

---
+++ app/src/test/kotlin/kttp/protocol/HttpRequestHandlerTest.kt
@@ -0,0 +1,50 @@
+package kttp.protocol
+
+import kttp.http.HttpClient
+import kttp.http.HttpRequestHandler
+import kttp.http.protocol.HttpHeaders
+import kttp.mock.SimpleTestServer
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.net.InetAddress
+import java.net.Socket
+import kotlin.concurrent.thread
+
+class HttpRequestHandlerTest {
+    private lateinit var simpleTestServer: SimpleTestServer
+    private val defaultPort = 80
+
+    @BeforeEach
+    fun setUp() {
+        simpleTestServer = SimpleTestServer(defaultPort)
+    }
+
+    @Test
+    fun createHttpServerWithInvalidPorts_shouldThrowException() {
+        thread {
+            Thread.sleep(500)
+//            HttpClient.get("http://google.com/", HttpHeaders(
+//                    "host" to "localhost"
+//            ))
+            HttpClient.get("http://localhost/", HttpHeaders(
+                    "Host" to "localhost"
+            ))
+        }
+
+        val acceptedClient = simpleTestServer.acceptSocket()
+        print(HttpRequestHandler().handle(acceptedClient))
+
+
+    }
+
+    @AfterEach
+    fun teardown() {
+        simpleTestServer.stop()
+    }
+
+
+    private fun createSocket(): Socket {
+        return Socket(InetAddress.getLocalHost(), defaultPort);
+    }
+}
