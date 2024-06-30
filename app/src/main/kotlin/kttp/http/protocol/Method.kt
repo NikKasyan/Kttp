fa9aa6322d8687a4361969c7d4815900c11592bc
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-06-30 11:02:08 +0200

    Add function to get Method by name

--- app/src/main/kotlin/kttp/http/protocol/Method.kt
+++ app/src/main/kotlin/kttp/http/protocol/Method.kt
@@ -11,10 +11,22 @@ enum class Method {
     TRACE,
     PATCH;
 
+    companion object {
+        fun byName(methodString: String): Method {
+
+            try {
+                return Method.valueOf(methodString)
+            } catch (e: IllegalArgumentException) {
+                throw UnknownHttpMethod("Unknown Method $methodString")
+            }
+        }
+
+    }
     fun allowsBody(): Boolean {
         return when (this) {
             POST, PUT, PATCH -> true
             else -> false
         }
     }
+
 }
