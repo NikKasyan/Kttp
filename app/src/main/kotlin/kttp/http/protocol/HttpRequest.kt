617c22f5558f3fe7b472c37988009be156241d55
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-06-30 11:27:07 +0200

    Fix uri of http request

--- app/src/main/kotlin/kttp/http/protocol/HttpRequest.kt
+++ app/src/main/kotlin/kttp/http/protocol/HttpRequest.kt
@@ -1,22 +1,48 @@
 package kttp.http.protocol
 
+import kttp.http.MissingHostHeader
 import java.net.URI
 
-class HttpRequest(private val requestLine: RequestLine, val httpHeaders: HttpHeaders = HttpHeaders(), val body: String = "") {
+class HttpRequest(
+    private val requestLine: RequestLine,
+    val httpHeaders: HttpHeaders = HttpHeaders(),
+    val body: String = ""
+) {
 
     companion object {
         fun from(method: Method, uri: URI, httpHeaders: HttpHeaders = HttpHeaders(), body: String = ""): HttpRequest {
+            if(uri.isAbsolute && uri.host == null && !httpHeaders.hasHost())
+                throw MissingHostHeader()
+            if(!httpHeaders.hasHost())
+                httpHeaders.withHost(uri.host)
+
             return HttpRequest(RequestLine(method, uri), httpHeaders, body)
         }
     }
 
     val httpVersion
         get() = requestLine.httpVersion
-    val requestUri
-        get() = requestLine.uri
+    val requestUri: URI
     val method
         get() = requestLine.method
 
+    init {
+        if (!httpHeaders.hasHost())
+            throw MissingHostHeader()
+        requestUri = combineToRequestUri(httpHeaders.getHost(), requestLine.requestTarget)
+    }
+
+    private fun combineToRequestUri(host: String, requestTarget: URI): URI {
+        return URI(
+            "http",
+            null,
+            host,
+            -1,
+            requestTarget.path,
+            requestTarget.query,
+            requestTarget.fragment
+        )
+    }
 
     override fun toString(): String {
         return "$requestLine$httpHeaders\r\n\r\n$body"
