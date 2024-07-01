9278bf07ffe8fc669b1b46b030b3207135cedd4e
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-07-01 22:38:14 +0200

    Implement streaming of http response

--- app/src/main/kotlin/kttp/http/protocol/HttpResponse.kt
+++ app/src/main/kotlin/kttp/http/protocol/HttpResponse.kt
@@ -1,7 +1,10 @@
 package kttp.http.protocol
 
-//TODO: body should not be string but Inputstream
-class HttpResponse(private val statusLine: StatusLine, val headers: HttpHeaders, private val body: HttpBody) {
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
+
+class HttpResponse(val statusLine: StatusLine, val headers: HttpHeaders, val body: HttpBody) {
 
     companion object {
         fun ok(headers: HttpHeaders = HttpHeaders(), body: String = ""): HttpResponse {
@@ -26,10 +29,84 @@ class HttpResponse(private val statusLine: StatusLine, val headers: HttpHeaders,
         if (body.hasContentLength() && !headers.hasContentLength())
             headers.withContentLength(body.contentLength!!)
         // Todo: handle Transfer-Encoding https://www.rfc-editor.org/rfc/rfc9112#name-transfer-encoding
-        TODO("Read body")
-        return "$statusLine\r\n$headers\r\n\r\n$body"
+        val bodyString = body.readAllBytes().toString(Charsets.UTF_8)
+        return "$statusLine\r\n$headers\r\n\r\n$bodyString"
+    }
+
+    fun asStream(): InputStream {
+        return HttpResponseStream(this)
     }
 
 
 
+}
+
+class HttpResponseStream(private val httpResponse: HttpResponse) : InputStream() {
+
+    private var closed = false;
+
+    private val statusLineAndHeaders: ByteArrayInputStream
+
+    init {
+        val statusLineBytes = httpResponse.statusLine.toString().toByteArray(Charsets.UTF_8)
+        val headersBytes = httpResponse.headers.toString().toByteArray(Charsets.UTF_8)
+        statusLineAndHeaders = ByteArrayInputStream(statusLineBytes + headersBytes)
+    }
+    override fun read(): Int {
+
+        val byteArray = ByteArray(1)
+        val read = read(byteArray, 0, 1)
+        return if (read == -1) -1 else byteArray[0].toInt()
+    }
+
+    override fun read(b: ByteArray): Int {
+        return read(b, 0, b.size)
+    }
+
+    override fun read(b: ByteArray, off: Int, len: Int): Int {
+        if (closed)
+            return -1
+
+        val statusLineAndHeadersRead = statusLineAndHeaders.read(b, off, len)
+        if (statusLineAndHeadersRead == -1) { // statusLineAndHeaders is empty
+            val bodyRead = httpResponse.body.read(b, off, len)
+            if (bodyRead == -1) {
+                closed = true
+                return -1
+            }
+            return bodyRead
+        } else if (statusLineAndHeadersRead < len) {
+            val bodyRead = httpResponse.body.read(b, off + statusLineAndHeadersRead, len - statusLineAndHeadersRead)
+            if (bodyRead == -1) {
+                closed = true
+                return -1
+            }
+            return statusLineAndHeadersRead + bodyRead
+        }
+        return statusLineAndHeadersRead
+    }
+
+    override fun close() {
+        closed = true
+        statusLineAndHeaders.close()
+        httpResponse.body.close()
+    }
+
+    override fun readAllBytes(): ByteArray {
+        val statusLineAndHeadersBytes = statusLineAndHeaders.readAllBytes()
+        val bodyBytes = httpResponse.body.readAllBytes()
+        return statusLineAndHeadersBytes + bodyBytes
+    }
+
+    override fun readNBytes(len: Int): ByteArray {
+        val statusLineAndHeadersBytes = statusLineAndHeaders.readNBytes(len)
+        val bodyBytes = httpResponse.body.readNBytes(len - statusLineAndHeadersBytes.size)
+        return statusLineAndHeadersBytes + bodyBytes
+    }
+
+    override fun readNBytes(b: ByteArray?, off: Int, len: Int): Int {
+        val statusLineAndHeadersBytes = statusLineAndHeaders.readNBytes(b, off, len)
+        val bodyBytes = httpResponse.body.readNBytes(b, off + statusLineAndHeadersBytes, len - statusLineAndHeadersBytes)
+        return statusLineAndHeadersBytes + bodyBytes
+    }
 }
