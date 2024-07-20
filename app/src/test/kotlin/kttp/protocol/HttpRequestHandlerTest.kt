fa141c0ee5b55930fd60a7154770ae245924b1ac
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-07-21 00:00:39 +0200

    Add test to check for request

--- app/src/test/kotlin/kttp/protocol/HttpRequestHandlerTest.kt
+++ app/src/test/kotlin/kttp/protocol/HttpRequestHandlerTest.kt
@@ -1,50 +1,32 @@
 package kttp.protocol
 
-import kttp.http.HttpClient
 import kttp.http.HttpRequestHandler
-import kttp.http.protocol.HttpHeaders
-import kttp.mock.SimpleTestServer
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import kttp.net.IOStream
 import org.junit.jupiter.api.Test
-import java.net.InetAddress
-import java.net.Socket
-import kotlin.concurrent.thread
+import java.io.ByteArrayOutputStream
+import java.io.OutputStream
+import java.io.StringWriter
 
 class HttpRequestHandlerTest {
-    private lateinit var simpleTestServer: SimpleTestServer
-    private val defaultPort = 80
-
-    @BeforeEach
-    fun setUp() {
-        simpleTestServer = SimpleTestServer(defaultPort)
-    }
 
     @Test
-    fun createHttpServerWithInvalidPorts_shouldThrowException() {
-        thread {
-            Thread.sleep(500)
-            //HttpClient.get("http://google.com/", HttpHeaders(
-            //        "host" to "localhost"
-            //))
-            HttpClient.get("http://localhost/", HttpHeaders(
-                    "Host" to "localhost"
-            ))
-        }
+    fun testRequest() {
+        val stream = """
+            GET / HTTP/1.1
+            Host: localhost:8080
+            User-Agent: TestClient/7.68.0
+            Accept: */*\r\n\r
+        """.trimIndent().byteInputStream()
 
-        val acceptedClient = simpleTestServer.acceptSocket()
-        val request = HttpRequestHandler().handle(acceptedClient.io)
+        val outputStream = ByteArrayOutputStream()
+        val ioStream = IOStream(stream, outputStream)
+
+        val request = HttpRequestHandler().handle(ioStream)
+
+        println(request)
+
 
 
     }
 
-    @AfterEach
-    fun teardown() {
-        simpleTestServer.stop()
-    }
-
-
-    private fun createSocket(): Socket {
-        return Socket(InetAddress.getLocalHost(), defaultPort);
-    }
 }
