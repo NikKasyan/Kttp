4cb07f0fc81a2d56782f6dd258dcbf749fcd0502
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2024-07-22 21:30:27 +0200

    Add new constructor for StatusLine to parse by String

--- app/src/main/kotlin/kttp/http/protocol/StatusLine.kt
+++ app/src/main/kotlin/kttp/http/protocol/StatusLine.kt
@@ -1,7 +1,31 @@
 package kttp.http.protocol
 
-data class StatusLine(val httpVersion: HttpVersion, val status: HttpStatus, val message: String = status.message) {
+class StatusLine {
+
+    val httpVersion: HttpVersion
+    val status: HttpStatus
+    val message: String
+
+    constructor(statusLine: String) {
+        val parts = statusLine.split(" ", limit = 3)
+        if (parts.size != 3)
+            throw InvalidHttpResponseLine("Status line must have the structure \"HTTP-Version Status-Code Reason-Phrase\". Invalid: $statusLine")
+        this.httpVersion = HttpVersion(parts[0])
+        this.status = HttpStatus.byCode(parts[1].toInt())
+        this.message = parts[2]
+    }
+
+    constructor(httpVersion: HttpVersion, status: HttpStatus, message: String = status.message) {
+        this.httpVersion = httpVersion
+        this.status = status
+        this.message = message
+    }
+
     override fun toString(): String {
         return "$httpVersion ${status.code} $message"
     }
 }
+
+class InvalidHttpResponseLine(msg: String) : InvalidHttpResponse(msg)
+
+open class InvalidHttpResponse(msg: String) : RuntimeException(msg)
