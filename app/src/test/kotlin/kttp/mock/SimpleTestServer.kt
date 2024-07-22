9675e822df377ac194713dfaa8834c162d3e5daa
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2024-07-22 21:30:04 +0200

    Add semaphore to ensure lateinit fields are actually initialized

--- app/src/test/kotlin/kttp/mock/SimpleTestServer.kt
+++ app/src/test/kotlin/kttp/mock/SimpleTestServer.kt
@@ -4,12 +4,14 @@ import kttp.net.ClientConnection
 import kttp.net.ConnectionOptions
 import java.net.ServerSocket
 import java.time.Duration
+import java.util.concurrent.Semaphore
 
 class SimpleTestServer(port: Int, private val timeout: Int = 0) {
 
     private val serverSocket = ServerSocket(port)
     private lateinit var clientConnection: ClientConnection
-        get
+
+    private val initSemaphore = Semaphore(0)
 
 
     //Just accept 1 client
@@ -18,21 +20,29 @@ class SimpleTestServer(port: Int, private val timeout: Int = 0) {
 
         val connectionOptions = ConnectionOptions(timeout = Duration.ofSeconds(timeout.toLong()))
         clientConnection = ClientConnection(socket, connectionOptions)
+        initSemaphore.release()
         return clientConnection
     }
 
     fun stop() {
         serverSocket.close()
-        if (::clientConnection.isInitialized)
-            clientConnection.close()
+        waitForInit()
+        clientConnection.close()
     }
 
     fun readLine(): String {
+        waitForInit()
         return clientConnection.io.readLine()
     }
 
     fun write(msg: String) {
+        waitForInit()
         clientConnection.io.writeln(msg)
+    }
+
+    private fun waitForInit() {
+        initSemaphore.acquire()
+        initSemaphore.release()
     }
 
 
