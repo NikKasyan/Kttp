b821150eac54b80b02ad583750c78a21f001acb8
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2024-07-22 22:25:46 +0200

    Refactor

--- app/src/main/kotlin/kttp/http/Main.kt
+++ app/src/main/kotlin/kttp/http/Main.kt
@@ -1,10 +1,29 @@
 package kttp.http
 
+import java.net.Socket
+
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val httpServer = HttpServer()
+//        val httpServer = HttpServer()
+//
+//        httpServer.start()
+        val socket = Socket("google.com", 80)
+        val outputStream = socket.getOutputStream()
+        val inputStream = socket.getInputStream()
 
-        httpServer.start()
+        outputStream.write("YEEEEEEEEEEEEEEEEEEEEEEEET / HTTP/1.1\r\nHost: google.com\r\n\r\n".toByteArray())
+        outputStream.flush()
+
+        val buffer = ByteArray(4096)
+        while (true) {
+            val bytesRead = inputStream.read(buffer)
+            if (bytesRead == -1)
+                break
+            print(String(buffer, 0, bytesRead))
+
+        }
+        println()
+
     }
 }
