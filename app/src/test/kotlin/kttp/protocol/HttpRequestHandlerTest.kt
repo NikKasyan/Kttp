c6b7acd99f6afb6c40ab9bca18e7d25dfa150526
Nikolaus Kasyan <nikolaus.kasyan@voxtronic.com>
2024-07-23 23:27:12 +0200

    Add Test if request is parsed correctly

--- app/src/test/kotlin/kttp/protocol/HttpRequestHandlerTest.kt
+++ app/src/test/kotlin/kttp/protocol/HttpRequestHandlerTest.kt
@@ -1,11 +1,15 @@
 package kttp.protocol
 
 import kttp.http.HttpRequestHandler
+import kttp.http.protocol.HttpHeaders
+import kttp.http.protocol.HttpVersion
+import kttp.http.protocol.Method
+import kttp.http.protocol.RequestLine
 import kttp.net.IOStream
 import org.junit.jupiter.api.Test
 import java.io.ByteArrayOutputStream
-import java.io.OutputStream
-import java.io.StringWriter
+import java.net.URI
+import kotlin.test.assertEquals
 
 class HttpRequestHandlerTest {
 
@@ -15,7 +19,7 @@ class HttpRequestHandlerTest {
             GET / HTTP/1.1
             Host: localhost:8080
             User-Agent: TestClient/7.68.0
-            Accept: */*\r\n\r
+            Accept: */*\r\n\r\n
         """.trimIndent().byteInputStream()
 
         val outputStream = ByteArrayOutputStream()
@@ -26,6 +30,31 @@ class HttpRequestHandlerTest {
         println(request)
 
 
+
+    }
+    @Test
+    fun testRequestIsParsedCorrectly(){
+        val path = "/"
+        val host = "localhost"
+        val port = 8080
+        val protocol = "http"
+        val requestLine = RequestLine(Method.GET, path, HttpVersion.DEFAULT_VERSION)
+        val headers = HttpHeaders()
+                      .withHost("$host:$port")
+                      .withUserAgent("TestClient/7.68.0")
+                      .withAccept("*/*")
+        val request = "$requestLine$headers\r\n"
+        val stream = request.byteInputStream()
+
+        val outputStream = ByteArrayOutputStream()
+        val ioStream = IOStream(stream, outputStream)
+
+        val parsedRequest = HttpRequestHandler().handle(ioStream)
+
+        assertEquals(requestLine.method, parsedRequest.method)
+        assertEquals(URI(protocol, null, host, 8080, path, null, null), parsedRequest.requestUri)
+        assertEquals(requestLine.httpVersion, parsedRequest.httpVersion)
+        assertEquals(headers, parsedRequest.httpHeaders)
 
     }
 
