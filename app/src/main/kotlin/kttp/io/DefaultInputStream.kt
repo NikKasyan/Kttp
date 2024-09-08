95f797a93726e6fdd39da097dcf2a7e7e491d84a
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-09-08 17:38:25 +0200

    Convert to UByte when reading single byte to fit contract

--- app/src/main/kotlin/kttp/io/DefaultInputStream.kt
+++ app/src/main/kotlin/kttp/io/DefaultInputStream.kt
@@ -6,7 +6,7 @@ abstract class DefaultInputStream: InputStream() {
     override fun read(): Int {
         val bytes = ByteArray(1)
         val read = read(bytes)
-        return if (read == -1) -1 else bytes[0].toInt()
+        return if (read == -1) -1 else bytes[0].toUByte().toInt()
     }
 
     abstract override fun read(bytes: ByteArray, offset: Int, length: Int): Int
---
+++ app/src/test/kotlin/kttp/net/DefaultInputStreamTest.kt
@@ -0,0 +1,29 @@
+package kttp.net
+
+import kttp.io.DefaultInputStream
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
+import java.io.InputStream
+
+class DefaultInputStreamTest {
+
+    @Test
+    fun testDefaultInputStream_SingleByteRead_ShouldReturnUnsignedByte() {
+        val inputStream = SimpleInputStream(ByteArrayInputStream(byteArrayOf(0xFF.toByte())))
+
+        val readByte = inputStream.read()
+
+        assertEquals(0xFF, readByte)
+    }
+
+
+}
+
+class SimpleInputStream(private val inputStream: InputStream): DefaultInputStream() {
+
+
+    override fun read(bytes: ByteArray, offset: Int, length: Int): Int {
+        return inputStream.read(bytes, offset, length)
+    }
+}
