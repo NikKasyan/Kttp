0fb365a63aa959f9fb48f6071182b25185fc6f1a
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2024-12-27 17:22:22 +0100

    Finish HttpClient

--- app/src/main/kotlin/kttp/http/HttpClient.kt
+++ app/src/main/kotlin/kttp/http/HttpClient.kt
@@ -1,8 +1,8 @@
 package kttp.http
 
-import kttp.http.protocol.HttpHeaders
-import kttp.http.protocol.HttpRequest
-import kttp.http.protocol.Method
+import kttp.http.protocol.*
+import kttp.io.IOStream
+import kttp.log.Logger
 import java.net.Socket
 import java.net.URI
 
@@ -11,35 +11,68 @@ class HttpClient(baseURL: String) {
     private val baseURI: URI
 
     companion object {
-        fun get(requestUrl: String, httpHeaders: HttpHeaders = HttpHeaders()) {
+        fun get(requestUrl: String, httpHeaders: HttpHeaders = HttpHeaders()): HttpResponse {
             return HttpClient(requestUrl).get(httpHeaders = httpHeaders)
         }
     }
 
+    private val log = Logger(javaClass)
+
     init {
-        this.baseURI = URI(baseURL)
+        this.baseURI = fixUri(URI(baseURL))
+
         if (!this.baseURI.isAbsolute)
             TODO("Throw real exception when baseURI is not absolute")
     }
 
-    fun get(requestUrl: String = "/", httpHeaders: HttpHeaders = HttpHeaders()) {
+    private fun fixUri(uri: URI): URI {
+        if (uri.scheme == null || !uri.scheme.startsWith("http"))
+            return URI("http://${uri}")
+        return uri
+    }
+
+    fun get(requestUrl: String = "/", httpHeaders: HttpHeaders = HttpHeaders()): HttpResponse {
         return request(Method.GET, requestUrl, httpHeaders)
     }
 
-    fun request(method: Method, requestUrl: String, httpHeaders: HttpHeaders = HttpHeaders()) {
+    fun request(method: Method, requestUrl: String, httpHeaders: HttpHeaders = HttpHeaders()): HttpResponse {
         val socket = Socket(baseURI.host, getPort())
-        val out = socket.getOutputStream()
+        val io = IOStream(socket.getInputStream(), socket.getOutputStream())
+
 
         val request = HttpRequest.from(method, URI(requestUrl), httpHeaders)
-        out.write(request.toString().toByteArray())
-        out.flush()
 
-        val input = socket.getInputStream().bufferedReader()
+        io.writeFromStream(request.asStream())
 
-        var line: String?
-        while ((input.readLine().also { line = it }) != null) {
-            println(line)
+
+        val statusLineString = io.readLine()
+        val statusLine = StatusLine(statusLineString)
+
+        log.debug{"StatusLine: $statusLine"}
+        val headers = readHeaders(io)
+
+        log.debug{"Headers: $headers"}
+
+        val body =
+            if(headers.hasTransferEncoding())
+                HttpBody.withTransferEncodingRequest(io, headers)
+            else
+                HttpBody(io, headers.contentLengthLong())
+
+        log.debug { "Body: $body" }
+
+        return HttpResponse(statusLine, headers, body)
+    }
+
+    private fun readHeaders(input: IOStream): HttpHeaders {
+        val headers = HttpHeaders()
+        while (true) {
+            val line = input.readLine()
+            if (line.isEmpty())
+                break
+            headers.add(line)
         }
+        return headers
     }
 
     private fun getPort(): Int {
@@ -49,6 +82,7 @@ class HttpClient(baseURL: String) {
             return 80
         if (baseURI.scheme == "https")
             return 443
+
         return -1
     }
 
