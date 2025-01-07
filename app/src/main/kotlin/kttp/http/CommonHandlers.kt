581c40d973d6d5c03d8c678a33c3ce6a67e13179
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2025-01-07 13:51:00 +0100

    Add better FileHosting Handler

--- app/src/main/kotlin/kttp/http/CommonHandlers.kt
+++ app/src/main/kotlin/kttp/http/CommonHandlers.kt
@@ -4,6 +4,8 @@ import kttp.http.protocol.HttpHeaders
 import kttp.http.protocol.HttpResponse
 import kttp.http.protocol.Method
 import kttp.http.protocol.MimeTypes
+import java.io.File
+import java.nio.file.Path
 import java.util.*
 
 
@@ -14,26 +16,51 @@ val NOT_FOUND_HANDLER = {
     }
 }
 
-val FileHost: (String) -> Handler = { root: String ->
-    {
-            val path = request.uri.path + "../"
+fun HostFilesHandler(relativeHostFilePath: String, requestBase: String = "") = HttpReqHandler(sanitizeRequestBase(requestBase), Method.GET, hostFiles(relativeHostFilePath, requestBase))
 
-            val file = java.io.File(root, path)
-            if (file.exists()) {
-                if (file.isDirectory) {
-                    val files = file.list() ?: arrayOf()
-                    val body = files.joinToString("\n") { "<li>$it</li>" }
-                    val headers = HttpHeaders{
-                        withContentType(MimeTypes.TEXT_HTML)
-                    }
-                    respond(HttpResponse.ok(headers, body = "<ul>$body</ul>"))
-
-                } else
-                    respond(HttpResponse.ok(body = file.readText()))
-            } else {
-                respond(HttpResponse.notFound(body = "Not Found $path"))
-            }
-
+private fun sanitizeRequestBase(requestBase: String): String {
+    val requestPath = if (requestBase.isEmpty() || requestBase == "/") {
+        ""
+    } else {
+        if (requestBase.endsWith("/")) {
+            requestBase
+        } else {
+            "$requestBase/"
+        }
+    }
+    return if(!requestPath.endsWith("**")){
+        "$requestPath**"
+    } else {
+        requestPath
     }
 }
 
+fun hostFiles(relativePath: String, requestBase: String = "") = hostFiles(Path.of(File(relativePath).absolutePath), requestBase)
+fun hostFiles(path: Path, requestBase: String = ""): Handler {
+    return Handler@{
+        val requestedPath = request.uri.path.removePrefix(requestBase)
+        val file = resolvePath(path, requestedPath).toFile()
+        if (file.exists()) {
+            if (file.isDirectory) {
+                val files = file.list() ?: arrayOf()
+                val body = files.joinToString("\n") { "<li>$it</li>" }
+                val headers = HttpHeaders{
+                    withContentType(MimeTypes.TEXT_HTML)
+                }
+
+                respond(HttpResponse.ok(headers, body = "<ul>$body</ul>"))
+
+            } else
+                respond(HttpResponse.ok(body = file.readText()))
+        } else {
+            respond(HttpResponse.notFound(body = "Not Found $requestedPath"))
+        }
+    }
+}
+
+fun resolvePath(path: Path, requestedPath: String): Path {
+    if(requestedPath.isEmpty() || requestedPath == "/")
+        return path
+    return path.resolve(requestedPath.removePrefix("/"))
+
+}
