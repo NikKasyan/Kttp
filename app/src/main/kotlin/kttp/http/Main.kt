75bcdaae5b09d362d32436630c43378bfa84474b
Nik Kasyan <16473991+NikKasyan@users.noreply.github.com>
2025-01-07 14:44:14 +0100

    Use better request handler

--- app/src/main/kotlin/kttp/http/Main.kt
+++ app/src/main/kotlin/kttp/http/Main.kt
@@ -1,11 +1,16 @@
 package kttp.http
 
+import kttp.http.protocol.HttpHeaders
+import kttp.security.SSL
+import java.nio.file.Paths
+
 
 object Main {
     @JvmStatic
     fun main(args: Array<String>) {
-        val httpServer = HttpServer(HttpServerOptions(secure = true))
-        httpServer.onGet("/test/**", FileHost("."))
+        val sslContext = SSL.createSSLContextFromCertificateAndKey("test.crt", "test.key")
+        val httpServer = HttpServer(HttpServerOptions(secure = true, tlsOptions = TLSOptions(sslContext = sslContext)))
+        httpServer.addRequestHandler(HostFilesHandler(".", "/test"))
         httpServer.start()
     }
 }
@@ -15,5 +20,8 @@ object Client {
     fun main(args: Array<String>) {
         val client = HttpClient("https://localhost", verifyCertificate = false)
         val response = client.get("/test")
+        println(response.statusLine)
+        println(response.body.readAsString())
+
     }
 }
